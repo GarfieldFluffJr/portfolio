ee89108113956923508db594eeb0fc21161dab0e
Louie <louielikescarrots2006@gmail.com>
2025-06-19 19:16:51 -0400

    Hero section done

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,35 +1,65 @@
-import { motion } from "framer-motion";
-import { FiArrowDown } from "react-icons/fi";
+import { FiGithub, FiLinkedin, FiMail } from "react-icons/fi";
 
 import styles from "../styles/modules/homePage.module.css";
 
+import SectionScrollIndicator from "../components/SectionScrollIndicator";
+
 export const Home = () => {
-  const SectionScrollIndicator = () => (
-    <div className="absolute bottom-6 left-1/2 transform -translate-x-1/2 z-20 block">
-      <motion.button
-        className="flex flex-col items-center justify-center opacity-60 hover:opacity-100 transition-opacity"
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 0.7, y: [0, 5, 0] }}
-        transition={{
-          y: {
-            repeat: Infinity,
-            duration: 2,
-            ease: "easeInOut",
-          },
-        }}
-      >
-        <span className={styles.sectionScrollText}>Scroll</span>
-        <FiArrowDown size={16} className={styles.sectionScrollText} />
-      </motion.button>
-    </div>
-  );
+  const handleSocialsClick = (social: string) => {
+    switch (social) {
+      case "GitHub":
+        window.open("https://github.com/GarfieldFluffJr", "_blank");
+        break;
+      case "LinkedIn":
+        window.open("https://www.linkedin.com/in/louieyin/", "_blank");
+        break;
+      case "Email":
+        const email = "louieyin6@gmail.com";
+        const subject = "I would like to connect";
+        const body = "Hi Louie, I would like to get in touch...";
+
+        const mailtoLink = `mailto:${email}?subject=${encodeURIComponent(
+          subject
+        )}&body=${encodeURIComponent(body)}`;
+        window.open(mailtoLink, "_blank");
+        break;
+      default:
+        window.open("");
+        console.error("Social was not found");
+    }
+  };
   return (
-    <div>
+    <div className="overflow-page">
+      {/* Main hero section */}
       <div className={styles.heroGrid}>
         <div className={styles.heroContainer}>
           <div className={styles.heroName}>Louie Yin</div>
-          <div className={styles.heroDesc}>SOFTWARE ENGINEER</div>
-          <div>Socials</div>
+          <div className={styles.heroDesc}>
+            SOFTWARE ENGINEER | VARSITY ATHLETE
+          </div>
+          <div className={styles.heroSocialsContainer}>
+            <div
+              className={styles.heroSocials}
+              onClick={() => handleSocialsClick("GitHub")}
+            >
+              <FiGithub className={styles.heroSocialItem} />
+              <span className={styles.heroSocialItem}>GitHub</span>
+            </div>
+            <div
+              className={styles.heroSocials}
+              onClick={() => handleSocialsClick("LinkedIn")}
+            >
+              <FiLinkedin className={styles.heroSocialItem} />
+              <span className={styles.heroSocialItem}>LinkedIn</span>
+            </div>
+            <div
+              className={styles.heroSocials}
+              onClick={() => handleSocialsClick("Email")}
+            >
+              <FiMail className={styles.heroSocialItem} />
+              <span className={styles.heroSocialItem}>Email</span>
+            </div>
+          </div>
         </div>
         <div>
           <SectionScrollIndicator />
