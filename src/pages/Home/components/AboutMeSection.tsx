3ab067f3844dd07385ac1754cfcd62cec78a9acd
Louie <louielikescarrots2006@gmail.com>
2025-06-21 22:34:52 -0400

    Refactored'

---
+++ src/pages/Home/components/AboutMeSection.tsx
@@ -0,0 +1,24 @@
+import styles from "../../../styles/modules/homePage.module.css";
+
+export default function AboutMeSection() {
+  return (
+    <div className={styles.aboutContainer}>
+      <h1 className={styles.sectionHeader}>About Me</h1>
+      <hr className={styles.sectionHeaderHr} />
+      <div className={styles.aboutContent}>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore
+        molestiae tenetur in ipsam dolore inventore odio, rem eligendi mollitia
+        quia accusamus impedit tempore obcaecati modi odit architecto numquam
+        illum est excepturi? Possimus necessitatibus consectetur nisi fugit
+        doloribus ipsa quod nemo voluptatibus fugiat, exercitationem quaerat
+        molestias ea accusantium quam nobis tempore similique tenetur
+        dignissimos expedita accusamus cupiditate culpa quasi illo. Deleniti
+        suscipit doloremque necessitatibus adipisci saepe placeat nesciunt!
+        Cumque vero commodi similique enim adipisci exercitationem? Inventore
+        repudiandae vel beatae dicta nemo nam suscipit, officiis aliquid
+        deleniti sunt illo labore, ratione sit eaque veritatis non assumenda
+        blanditiis sed laborum illum libero sint.
+      </div>
+    </div>
+  );
+}
