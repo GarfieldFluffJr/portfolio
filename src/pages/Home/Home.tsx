cca2ac4196a55e2a4aaae7e8a446bb318d5394aa
Louie <louielikescarrots2006@gmail.com>
2025-06-25 12:46:50 -0400

    Technical Skills Card

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -4,6 +4,8 @@ import SectionScrollIndicator from "../../components/SectionScrollIndicator";
 import HeroSection from "./components/HeroSection";
 import ProjectsSection from "./components/ProjectsSection";
 
+import { FiCode, FiDatabase, FiMonitor } from "react-icons/fi";
+
 export const Home = () => {
   return (
     <div>
@@ -38,18 +40,95 @@ export const Home = () => {
             sint.
           </div>
         </div>
-        <div>
-          <SectionScrollIndicator />
-        </div>
+        <SectionScrollIndicator />
       </div>
 
       {/* Projects Section */}
       <div className={styles.sectionContainer}>
         <ProjectsSection />
-        <div>
-          <SectionScrollIndicator />
+        <SectionScrollIndicator />
+      </div>
+
+      {/* Technical Skills Section */}
+      <div className={styles.sectionContainer}>
+        <div className={styles.sectionInnerContainer}>
+          <h1 className={styles.sectionHeader}>Technical Skills</h1>
+          <div className={styles.sectionDescriptionContainer}>
+            <hr className={styles.sectionHeaderHr} />
+            <div className={styles.sectionDescription}>
+              The technologies and tools and I use and harness.
+            </div>
+          </div>
+
+          <div className={styles.skillsContainer}>
+            <div className={styles.skillsInnerContainer}>
+              <div className={`${styles.skillsFocusCard} card`}>
+                <div className={styles.skillsFocusHeader}>
+                  <div className={styles.skillsFocusIcon}>
+                    <FiCode size={22} />
+                  </div>
+                  <span>Frontend Development</span>
+                </div>
+                <div>
+                  <div className={styles.skillsFocusWrapper}>
+                    <div className={styles.skillsFocusDescriptionWrapper}>
+                      <span className={styles.skillsFocusSkills}>
+                        React, Next.js
+                      </span>
+                      <span className={styles.skillsFocusDescription}>
+                        Advanced
+                      </span>
+                    </div>
+                    <div className={styles.skillsFocusProgressContainer}>
+                      <div
+                        className={styles.skillsFocusProgressBar}
+                        style={{ width: `95%` }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className={`${styles.skillsFocusCard} card`}>
+                <div className={styles.skillsFocusHeader}>
+                  <div className={styles.skillsFocusIcon}>
+                    <FiDatabase size={22} />
+                  </div>
+                  <span>Backend Development</span>
+                </div>
+                <div>
+                  <div className={styles.skillsFocusWrapper}>
+                    <div className={styles.skillsFocusDescriptionWrapper}>
+                      <span className={styles.skillsFocusSkills}>
+                        React, Next.js
+                      </span>
+                      <span className={styles.skillsFocusDescription}>
+                        Advanced
+                      </span>
+                    </div>
+                    <div className={styles.skillsFocusProgressContainer}>
+                      <div
+                        className={styles.skillsFocusProgressBar}
+                        style={{ width: `95%` }}
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className={styles.skillsInnerContainer}>
+              <div className={`${styles.skillsAllSkills} card`}>
+                <div className={styles.skillsFocusHeader}>
+                  <div className={styles.skillsFocusIcon}>
+                    <FiMonitor size={22} />
+                  </div>
+                  <span>Technologies</span>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+      <SectionScrollIndicator />
     </div>
   );
 };
