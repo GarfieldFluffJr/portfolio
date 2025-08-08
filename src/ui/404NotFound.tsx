8e5d1980bd845ff9a1d01a5590f5caa6e0a3236b
Louie <louielikescarrots2006@gmail.com>
2025-08-08 17:37:32 -0400

    not found page

--- src/ui/404NotFound.tsx
+++ src/ui/404NotFound.tsx
@@ -1,3 +1,18 @@
+import { FiArrowLeft } from "react-icons/fi";
+import { NavLink } from "react-router-dom";
+
 export default function NotFound() {
-  return <div>Page Not Found</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="card p-6 rounded shadow text-center">
+        404. Oops, this page was not found!
+      </div>
+      <div>
+        <NavLink to="/" className="big-black-button">
+          <FiArrowLeft size={16} />
+          <span>Back to Home Page</span>
+        </NavLink>
+      </div>
+    </div>
+  );
 }
