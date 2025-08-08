8ee8e54b93fa95dffcb257f022dfa7a59a65cff9
Louie <louielikescarrots2006@gmail.com>
2025-08-08 01:18:21 -0400

    footer icon links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,31 @@
+import { FiGithub, FiLinkedin } from "react-icons/fi";
+
+import styles from "../styles/modules/footer.module.css";
+
 export default function Footer() {
   return (
-    <footer className="footer">
-      © {new Date().getFullYear()} Louie Yin. Made with ❤️
+    <footer className={styles.footer}>
+      <div className={styles.iconContainer}>
+        <a
+          href="https://github.com/GarfieldFluffJr"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.icon}
+        >
+          <FiGithub size={20} />
+          {}
+        </a>
+        <a
+          href="https://www.linkedin.com/in/louieyin/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className={styles.icon}
+        >
+          <FiLinkedin size={20} />
+          {}
+        </a>
+      </div>
+      <span>© {new Date().getFullYear()} Louie Yin. Made with ❤️</span>
     </footer>
   );
 }
