dea0563692e564a0b7f6e5480bcc7944b05671ec
Louie <louielikescarrots2006@gmail.com>
2025-08-09 12:51:35 -0400

    vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import AppRouter from "./AppRouter.tsx";
+import { Analytics } from "@vercel/analytics/next";
 
 import "./styles/globals.css";
 import "./styles/colours.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Analytics />
     <AppRouter />
   </StrictMode>
 );
