0f1e93898ab792b53188ffa5e8ef05a9e843965d
Louie <louielikescarrots2006@gmail.com>
2025-08-09 16:18:20 -0400

    display contact page with header

--- frontend/src/pages/Contact/Contact.tsx
+++ frontend/src/pages/Contact/Contact.tsx
@@ -1,3 +1,12 @@
+import styles from "../../styles/modules/contactPage.module.css";
+
 export const ContactHome = () => {
-  return <div>Contact Page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.subContainer}>
+        <h1 className={styles.title}>Contact Me</h1>
+        <hr className={styles.titleHR} />
+      </div>
+    </div>
+  );
 };
