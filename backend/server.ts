d3bb6d9971fb5177c200ca1f2c1f0b96bf31e7ec
Louie <louielikescarrots2006@gmail.com>
2025-08-10 22:04:42 -0400

    basic running node server

---
+++ backend/server.ts
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Global middleware
+app.use(cors()); // TODO: change after production to url, right now accepts every origin
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello from Express");
+});
+
+app.post("/data", (req, res) => {
+  console.log(res.body);
+  res.json({ message: "Data received", yourData: req.body });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
