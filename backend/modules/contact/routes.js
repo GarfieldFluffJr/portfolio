79746b8e297568b36419d8b0d52864e771cb4f6a
Louie <louielikescarrots2006@gmail.com>
2025-08-15 12:49:49 -0400

    rate limiter

--- backend/modules/contact/routes.js
+++ backend/modules/contact/routes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const contactRouter = express.Router();
 
+const rateLimit = require("express-rate-limit");
+const rateLimiter = rateLimit({
+  windowMs: 24 * 60 * 60 * 1000, // 1 day
+  max: 10,
+  message:
+    "You have reached the daily limit for sending emails. Please try again tomorrow.",
+});
+
 const { sendEmail } = require("./controller");
 
-contactRouter.post("/send", sendEmail);
+contactRouter.post("/send", rateLimiter, sendEmail);
 
 module.exports = contactRouter;
--- backend/modules/test/routes.js
+++ backend/modules/test/routes.js
@@ -4,6 +4,6 @@ const testRouter = express.Router();
 const { testReturnData, testGet } = require("./controller");
 
 testRouter.post("/return-data", testReturnData);
-testRouter.get("/get-data", testGet)
+testRouter.get("/get-data", testGet);
 
 module.exports = testRouter;
