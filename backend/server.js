a509442f8cccf439bc99c3783c7af1bd6959add1
Louie <louielikescarrots2006@gmail.com>
2025-08-15 11:56:00 -0400

    fixed port

--- backend/server.js
+++ backend/server.js
@@ -16,5 +16,5 @@ app.use("/test", testRoute);
 app.use("/contact", contactRoute);
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
