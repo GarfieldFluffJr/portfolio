454d52bbc07fb0719411710b329a89957172c8b3
Louie <louielikescarrots2006@gmail.com>
2025-08-15 12:34:01 -0400

    VITEURL

--- frontend/src/api/http.ts
+++ frontend/src/api/http.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const URL = process.env.URL || "http://localhost:3000";
+const URL = import.meta.env.VITE_URL || "http://localhost:3000";
 
 export const http = axios.create({
   baseURL: URL,
