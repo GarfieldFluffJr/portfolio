f963d345cf6fe22cad75db3ecbb88eb4b6d62c4c
Louie <louielikescarrots2006@gmail.com>
2025-08-16 00:51:17 -0400

    swim page colour switch + header

--- frontend/src/app/pages/Swim/Swim.tsx
+++ frontend/src/app/pages/Swim/Swim.tsx
@@ -1,3 +1,19 @@
+import styles from "../../styles/modules/swimPage.module.css";
+
 export const SwimHome = () => {
-  return <div>Swim Page</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.subContainer}>
+        <h1 className={styles.title}>My Swim Career</h1>
+        <hr className={styles.titleHR} />
+        <div className={styles.titleDesc}>
+          Welcome to my swim page! Here, you can learn all about my personal
+          swim journey along with all of my achievements and milestones.
+        </div>
+      </div>
+
+      {/* Content will go here */}
+      <div>Swim content coming soon...</div>
+    </div>
+  );
 };
