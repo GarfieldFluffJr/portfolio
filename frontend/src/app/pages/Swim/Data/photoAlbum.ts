d13f2b111fe29b559a509b0cdc84a39ac7fcb3be
Louie <louieyin6@gmail.com>
2025-09-03 18:51:25 -0400

    UW First Year Album data

--- frontend/src/app/pages/Swim/Data/photoAlbum.ts
+++ frontend/src/app/pages/Swim/Data/photoAlbum.ts
@@ -21,3 +21,25 @@ export const photoAlbumCovers: PhotoAlbumCover[] = [
     destination: "/Curacao_2024",
   },
 ];
+
+type PhotoAlbum = {
+  title: string;
+  photos: string[];
+};
+
+export const UW_First_Year: PhotoAlbum = {
+  title: "UW First Year (2024-2025)",
+  photos: [
+    "/images/Swim/UW First Year/1.JPG",
+    "/images/Swim/UW First Year/2.JPEG",
+    "/images/Swim/UW First Year/3.png",
+    "/images/Swim/UW First Year/4.JPEG",
+    "/images/Swim/UW First Year/5.JPEG",
+    "/images/Swim/UW First Year/6.JPG",
+    "/images/Swim/UW First Year/7.png",
+    "/images/Swim/UW First Year/8.JPEG",
+    "/images/Swim/UW First Year/9.jpg",
+    "/images/Swim/UW First Year/10.jpg",
+    "/images/Swim/UW First Year/11.JPEG",
+  ],
+};
